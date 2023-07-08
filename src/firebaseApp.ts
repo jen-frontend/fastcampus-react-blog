a28be6edeea41a89616457ab3c1fee387793f210
jen-frontend <jen.frontend@gmail.com>
2023-07-09 00:19:23 +0900

    feat: firebase 세팅

---
+++ src/firebaseApp.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENTER_ID,
+  appId: process.env.REACT_APP_ID,
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import firebase from "firebaseApp";
+
+console.log(firebase);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
