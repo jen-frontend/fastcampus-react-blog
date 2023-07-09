7445d968dc685afa5c70ca85ca1f5a0799f900c1
jen-frontend <jen.frontend@gmail.com>
2023-07-09 18:38:51 +0900

    feat: Firestore로 데이터 가져오기: 게시글 상세 작업

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,40 +1,61 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { PostProps } from "./PostList";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import Loader from "./Loader";
 
 export default function PostDetail() {
+  const [post, setPost] = useState<PostProps | null>(null);
+  const params = useParams();
+
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  const handleDelete = () => {
+    console.log("delete!");
+  };
+
+  useEffect(() => {
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+
   return (
     <>
       <div className="post__detail">
-        <div className="post__box">
-          <div className="post__title">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </div>
-          <div className="post__profile-box">
-            <div className="post__profile" />
-            <div className="post__author-name">패스트캠퍼스</div>
-            <div className="post__date">2023.07.08 토요일</div>
-          </div>
-          <div className="post__utils-box">
-            <div className="post__delete">삭제</div>
-            <div className="post__edit">
-              <Link to={`/posts/edit/1`}>수정</Link>
+        {post ? (
+          <div className="post__box">
+            <div className="post__title">{post?.title}</div>
+            <div className="post__profile-box">
+              <div className="post__profile" />
+              <div className="post__author-name">{post?.email}</div>
+              <div className="post__date">{post?.createdAt}</div>
+            </div>
+            <div className="post__utils-box">
+              <div
+                className="post__delete"
+                role="presentation"
+                onClick={handleDelete}
+              >
+                삭제
+              </div>
+              <div className="post__edit">
+                <Link to={`/posts/edit/1`}>수정</Link>
+              </div>
+            </div>
+            <div className="post__text post__text--pre-wrap">
+              {post?.content}
             </div>
           </div>
-          <div className="post__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget
-            pretium nunc. Duis sed arcu eget leo maximus fermentum vel ut risus.
-            Morbi quis enim consequat, venenatis mauris a, bibendum justo.
-            Vestibulum eros risus, feugiat eget auctor et, lacinia in quam.
-            Pellentesque pellentesque nunc ultricies nulla convallis finibus.
-            Nulla interdum laoreet ex, id euismod arcu. Vestibulum pulvinar sem
-            dolor, in laoreet nunc faucibus eu. Suspendisse convallis vehicula
-            turpis ut dignissim. Sed nec turpis suscipit, ultricies nisi non,
-            ullamcorper diam. Duis non ullamcorper enim. Suspendisse egestas
-            lorem sed enim gravida, sit amet ultricies ligula dignissim.
-            Praesent nec consequat est. Praesent interdum blandit dolor, a
-            dapibus erat gravida ut. Maecenas blandit diam est, ut viverra augue
-            faucibus ut. Curabitur varius vestibulum erat sit amet malesuada.
-          </div>
-        </div>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -10,8 +10,8 @@ interface PostListProps {
 
 type TabType = "all" | "my";
 
-interface PostProps {
-  id: string;
+export interface PostProps {
+  id?: string;
   title: string;
   email: string;
   summary: string;
@@ -68,7 +68,7 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
                   <div className="post__date">{post?.createdAt}</div>
                 </div>
                 <div className="post__title">{post?.title}</div>
-                <div className="post__text">{post?.content}</div>
+                <div className="post__text">{post?.summary}</div>
               </Link>
               {post?.email === user?.email && (
                 <div className="post__utils-box">
