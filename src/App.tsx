0a6a0aa2330d55c1a4ea7f6e4e7266df27f4dc2c
jen-frontend <jen.frontend@gmail.com>
2023-07-09 14:21:43 +0900

    feat: 로그인, 회원가입 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Router from "./components/Router";
+import Loader from "components/Loader";
 
 function App() {
   const auth = getAuth(app);
-
+  // auth를 체크하기 전에 (initialize 전)에는 loader를 띄워주는 용도
+  const [init, setInit] = useState<boolean>(false);
+  // auth의 currentUser가 있으면 authenticated로 변경
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
-  return <Router isAuthenticated={isAuthenticated} />;
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
+  return (
+    <>
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
+    </>
+  );
 }
 
 export default App;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -24,8 +24,6 @@ export default function Router({ isAuthenticated }: RouterProps) {
             <Route path="/posts/new" element={<PostNew />} />
             <Route path="/posts/edit/:id" element={<PostEdit />} />
             <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
             <Route path="*" element={<Navigate replace to="/" />} />
           </>
         ) : (
