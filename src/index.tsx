451ad840f197136c46cc9eff8cc4b13581917b97
jen-frontend <jen.frontend@gmail.com>
2023-07-09 21:56:24 +0900

    feat: 다크모드 toggle 기능 구현

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "context/AuthContext";
+import { ThemeContextProvider } from "context/ThemeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <AuthContextProvider>
-    <Router>
-      <App />
-    </Router>
-  </AuthContextProvider>
+  <ThemeContextProvider>
+    <AuthContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthContextProvider>
+  </ThemeContextProvider>
 );
