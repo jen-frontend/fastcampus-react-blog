4f43b11168a553e65a43a9e4321d5fd5dfbc0638
jen-frontend <jen.frontend@gmail.com>
2023-07-09 22:18:26 +0900

    feat: 다크모드 toggle css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ThemeContext from "context/ThemeContext";
 
 import Router from "./components/Router";
 import Loader from "components/Loader";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   // auth를 체크하기 전에 (initialize 전)에는 loader를 띄워주는 용도
   const [init, setInit] = useState<boolean>(false);
@@ -28,10 +30,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
